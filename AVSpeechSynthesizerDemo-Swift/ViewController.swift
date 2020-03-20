1915f2af7cc348cacc1e6e856536f1d99bc05463
tongwanming <359209162@qq.com>
2020-03-20 10:13:10 +0800

    Initial Commit

---
+++ AVSpeechSynthesizerDemo-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVSpeechSynthesizerDemo-Swift
+//
+//  Created by 童万明 on 2020/3/20.
+//  Copyright © 2020 童万明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
